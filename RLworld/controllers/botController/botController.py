698833ffbe8210495d9d1b87e32591887094894b
Sahil177 <sahil0121@live.com>
2021-10-30 23:40:22 +0100

    outline of robot class created

--- RLworld/controllers/botController/botController.py
+++ RLworld/controllers/botController/botController.py
@@ -1,32 +1,63 @@
 """botController controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+import sys
+packages = [r'C:\\Users\\sahil\\Documents\\Python_environments\\env37', r'C:\\Users\\sahil\\Documents\\Python_environments\\env37\\lib\\site-packages']
+for path in packages:
+    sys.path.append(path)
 
-# create the Robot instance.
-robot = Robot()
+from deepbots.robots.controllers.robot_emitter_receiver_csv import RobotEmitterReceiverCSV
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+class bot(RobotEmitterReceiverCSV):
+    def __init__(self):
+        super().__init__()
+        self.main_gps = self.robot.getDevice("gps_main")
+        self.main_gps.enable(self.get_timestep())
+        self.ds_left = self.robot.getDevice("ds_left")
+        self.ds_left.enable(self.get_timestep())
+        self.ds_right = self.robot.getDevice("ds_right")
+        self.ds_right.enable(self.get_timestep())
+        self.colour_sensor = self.robot.getDevice("camera")
+        self.emitter = self.robot.getDevice("emitter")
+        self.reciever = self.robot.getDevice("reciever")
+        self.reciever.enable(self.get_timestep())
+        self.wheel1 = self.robot.getDevice("wheel1")
+        self.wheel1.setPosition(float('inf'))
+        self.wheel1.setVelocity(0.0)
+        self.wheel2 = self.robot.getDevice("wheel2")
+        self.wheel2.setPosition(float('inf'))
+        self.wheel2.setVelocity(0.0)
+        self.gripper_left_motor = self.robot.getDevice("Lmotor")
+        self.gripper_right_motor = self.robot.getDevice("Rmotor")
+        self.gripper_left_motor.setAvailableTorque(0.2)
+        self.gripper_right_motor.setAvailableTorque(0.2)
+        self.gripper_left_motor.setPosition(0.1)
+        self.gripper_right_motor.setPosition(0.1)
+        self.robot_name = self.robot.getName()
+        if self.robot_name == 'red':
+            self.robot_id = 0
+            self.emitter.setChannel(0)
+            self.reciever.setChannel(0)
+        else:
+            self.robot_id = 1
+            self.emitter.setChannel(1)
+            self.reciever.setChannel(1)
+    
+    def grip(self, grip_pos):
+        if grip_pos ==1:
+            self.gripper_left_motor.setPosition(0.1)
+            self.gripper_right_motor.setPosition(0.1)
+        else:
+            self.gripper_left_motor.setPosition(0.9)
+            self.gripper_right_motor.setPosition(0.9)
+    
+    def create_message(self):
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+        return None
+    
+    def use_message_data(self, message):
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+        return None
 
-    # Process sensor data here.
+bot_controller = bot()
+bot_controller.run()
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
